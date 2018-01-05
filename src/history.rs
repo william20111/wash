346e666fff8a856bcf0a1ec03f8e2d691c239582
William Fleming <wfleming@grumpysysadm.com>
2018-01-05 17:16:49 +0000

    working autocreation of history file if doesnt exist

--- src/history.rs
+++ src/history.rs
@@ -6,6 +6,7 @@ use std::env;
 use std::thread;
 use std::fs::OpenOptions;
 use std::time::{SystemTime, UNIX_EPOCH};
+use std::path::Path;
 
 
 pub struct History {
@@ -15,21 +16,28 @@ pub struct History {
 impl History {
     pub fn setup_history() -> String {
         let home = env::home_dir().unwrap();
-        format!("{}/{}", home.to_str().unwrap(), ".wash_history")
+        let history_file = format!("{}/{}", home.to_str().unwrap(), ".wash_history");
+        if Path::new(&history_file).exists() {
+            history_file
+        } else {
+            fs::File::create(&history_file).unwrap();
+            history_file
+        }
     }
 
     pub fn write_history(hf: String, input: String) {
         thread::spawn(move || {
             let mut history = OpenOptions::new()
-            .append(true)
-            .create(true)
-            .open(hf).unwrap();
+                .append(true)
+                .create(true)
+                .open(hf)
+                .unwrap();
             match SystemTime::now().duration_since(UNIX_EPOCH) {
                 Ok(n) => {
                     let h = format!("{}: {}", n.as_secs(), input);
                     history.write(h.as_bytes());
                 }
-                Err(e) => {},
+                Err(e) => {}
             }
         });
     }
