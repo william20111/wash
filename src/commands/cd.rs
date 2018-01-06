92f7b7022c8d427f1d5c8ce13ba5e937447b1556
William Fleming <wfleming@grumpysysadm.com>
2018-01-06 18:51:32 +0000

    cd prints OS error on directory move fail, ls prints on single line rather than list

--- src/commands/cd.rs
+++ src/commands/cd.rs
@@ -2,5 +2,8 @@ use std::fs;
 use std::env;
 
 pub fn exec(dir: String) {
-    env::set_current_dir(dir);
+    match env::set_current_dir(dir) {
+        Ok(_) => {},
+        Err(e) => {println!("{}", e);}
+    }
 }
--- src/commands/ls.rs
+++ src/commands/ls.rs
@@ -2,8 +2,11 @@ use std::fs;
 
 pub fn exec(dir: String) {
     let paths = fs::read_dir("./").unwrap();
-
+    let mut files = String::new();
     for path in paths {
-        println!("{}", path.unwrap().path().display())
+        //println!("{}", path.unwrap().path().display())
+        files.push_str(path.unwrap().path().file_name().unwrap().to_str().unwrap());
+        files.push_str("  ");
     }
+    println!("{}", files)
 }
